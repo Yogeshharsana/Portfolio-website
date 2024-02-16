ed3390cff519156c23fe7eb31bcbabb2c0f80534
yogesh harsana <e20cse331@bennett.edu.in>
2024-02-16 17:20:55 +0530

    updates

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,9 +1,7 @@
 import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.png";
-import Work6 from "../../assets/work6.jpg";
+
 
 export const projectsData = [
   {
@@ -15,31 +13,13 @@ export const projectsData = [
   {
     id: 2,
     image: Work2,
-    title: "Company's Landing Page",
-    link: "https://mg-export.vercel.app/",
-  },
-  {
-    id: 3,
-    image: Work3,
-    title: "Hotel Admin Management System",
-    link: "https://hotel-management-system-main.vercel.app/",
-  },
-  {
-    id: 4,
-    image: Work4,
-    title: "Landing Page",
-    link: "https://long-island-web-design.netlify.app/",
+    title: "Github Repository Listing Webpage",
+    link: "https://fyle28.netlify.app/",
   },
   {
     id: 5,
-    image: Work5,
+    image: Work3,
     title: "Portfolio",
-    link: "https://uy24.netlify.app/",
-  },
-  {
-    id: 6,
-    image: Work6,
-    title: "Automatic Gmail Replier",
-    link: "https://github.com/UY24/auto-mail-reply-using-Node.Js",
-  },
+    link: "https://yogeshharsana.netlify.app/",
+  }
 ];
